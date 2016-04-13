86cf4f2e3c04524149a5d14e5e1c8288b724694e
Alexandra F <thunderlive-sunshine@live.com>
2016-04-13 08:51:17 -0400

    Update with comments

--- one.ino
+++ one.ino
@@ -8,24 +8,28 @@ int state;
 void setup() {
   pinMode(13, OUTPUT);
   pinMode(2, INPUT);
-  //pinMode(2, INPUT_PULLUP);
+  
+  // To set pull-up resistor:
+  // pinMode(2, INPUT_PULLUP);
 }
 
 void loop() {
   
+  // Read switch value (3)
   int rd = digitalRead(2);
   
+  // Debounce (4)
   if( rd != last ) ldb=millis();
   
   if( (millis()-ldb) > dl ){
     
-    //int state = digitalRead(2);
     if( rd != state ){
       state = rd;
       if( state == HIGH ) on=!on;
     }
   }
   
+  // Light up (2)
   if( on ) digitalWrite(13, HIGH);
   else digitalWrite(13, LOW);
   
