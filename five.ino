5a3027264788819bb0b974f72b5920e35e106d91
Alexandra F <thunderlive-sunshine@live.com>
2016-04-13 08:52:02 -0400

    Import project for #5

---
+++ five.ino
@@ -0,0 +1,18 @@
+// UNFINISHED
+
+void setup() {
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+
+}
+
+void light(int pin){
+  digitalWrite(pin, HIGH); 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
