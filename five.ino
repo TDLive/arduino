3b842ed6c0e5a62d79df68abf62011cf1964f21e
Alexandra F <thunderlive-sunshine@live.com>
2016-04-15 08:46:20 -0400

    Finish 5-6

--- five.ino
+++ five.ino
@@ -1,18 +1,45 @@
 // UNFINISHED
 
+int order[4] = { 11, 13, 12, 10 };
+
 void setup() {
-  pinMode(13, OUTPUT);
-  pinMode(12, OUTPUT);
-  pinMode(11, OUTPUT);
-  pinMode(10, OUTPUT);
+  
+  for( int i=0; i<4; i++ ){
+    pinMode(order[i], OUTPUT);
+  }
 
 }
 
-void light(int pin){
+void on(int pin){
   digitalWrite(pin, HIGH); 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void off(int pin){
+  digitalWrite(pin, LOW); 
+}
 
+void loop() {
+  for( int i=0; i<4; i++ ){
+    on(order[i]);
+    delay(50);
+    off(order[i]); 
+  }
+  
+  for( int i=4; i>-1; i-- ){
+    on(order[i]);
+    delay(50);
+    off(order[i]);
+  }
+  
+  for( int i=0; i<255; i++ ){
+    analogWrite(9, i);
+    delay(5);
+  }
+  
+  for( int i=250; i>0; i-- ){
+    analogWrite(9, i);
+    delay(5);
+  }
+  
+                       
 }
